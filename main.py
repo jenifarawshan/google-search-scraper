3063efa9baf5929f592f484b2cbf785b2958ea26
Jenifa Rawshan <jenifarawshan057@gmail.com>
2022-09-13 00:29:11 +0600

    feat: create multiple functions for separation of concerns

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from requests_html import HTMLSession
 
 
-def main():
-    session = HTMLSession()
-    response = session.get(
-        url="https://google.com/search", params={"q": "music video"})
+GOOGLE_SEARCH_URL = "https://google.com/search"
+GOOGLE_SEARCH_PARAM = "q"
+GOOGLE_SEARCH_RESULT_WRAPPER_SELECTOR = "div.g"
+GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR = "video-voyager"
 
-    if response and not hasattr(response, "html"):
-        return
 
-    website_search_result_list = response.html.find("div.g")
+def parse_website_search_results(html_elem):
+    website_search_result_list = html_elem.find(
+        GOOGLE_SEARCH_RESULT_WRAPPER_SELECTOR)
 
     for website_search_result in website_search_result_list:
-        if website_search_result.element.getparent().tag == "video-voyager":
+        if website_search_result.element.getparent().tag == GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR:
             continue
 
         website_link_elem = website_search_result.find("a", first=True)
@@ -24,14 +24,23 @@ def main():
             website_title = website_link_elem.find("h3", first=True).text
         elif website_search_result.find("h3:last-child", first=True):
             website_title = website_title = website_search_result.find(
-                "h3:last-child", first=True).text
+                "h3:last-child",
+                first=True
+            ).text
 
         if not all([website_title, website_link]):
             continue
 
-        print(website_title, website_link)
+        return {
+            "title": website_title,
+            "link": website_link
+        }
 
-    video_search_result_list = response.html.find("video-voyager")
+
+def parse_video_search_results(html_elem):
+    video_search_result_list = html_elem.find(
+        GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR
+    )
 
     for video_search_result in video_search_result_list:
         video_link_elem = video_search_result.find("a", first=True)
@@ -39,16 +48,22 @@ def main():
 
         if video_search_result.find("a[aria-label]", first=True):
             video_thumbnail_elem = video_search_result.find(
-                "a[aria-label]", first=True)
+                "a[aria-label]",
+                first=True
+            )
         else:
             video_thumbnail_elem = video_search_result.find(
-                "div[aria-hidden='true']", first=True)
+                "div[aria-hidden='true']",
+                first=True
+            )
 
         video_title = ""
 
         if video_link_elem.find("div[role='heading'] span", first=True):
             video_title = video_link_elem.find(
-                "div[role='heading'] span", first=True).text
+                "div[role='heading'] span",
+                first=True
+            ).text
         elif video_link_elem.find("h3", first=True):
             video_title = video_link_elem.find("h3", first=True).text
 
@@ -56,12 +71,32 @@ def main():
 
         if video_thumbnail_elem.find("div[role='presentation']", first=True):
             video_duration = video_thumbnail_elem.find(
-                "div[role='presentation']", first=True).text
+                "div[role='presentation']",
+                first=True
+            ).text
 
         if not all([video_title, video_duration, video_link]):
             continue
 
-        print(video_title, video_duration, video_link)
+        return {
+            "title": video_title,
+            "duration": video_duration,
+            "link": video_link
+        }
+
+
+def main():
+    session = HTMLSession()
+    search_query = "music video"
+    response = session.get(
+        url=GOOGLE_SEARCH_URL, params={GOOGLE_SEARCH_PARAM: search_query}
+    )
+
+    if response and not hasattr(response, "html"):
+        return
+
+    website_search_result_list = parse_website_search_results(response.html)
+    video_search_result_list = parse_video_search_results(response.html)
 
 
 if __name__ == "__main__":
