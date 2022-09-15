a8d5e85c6b8ea643dceaa5beec0ddbe3815f139c
jenifarawshan <jenifar@inneed.cloud>
2022-09-15 22:31:14 +0600

    feat: add command line argument support

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from collections import defaultdict
-from requests_html import HTMLSession, urlparse
 from datetime import datetime
 import time
 import json
 import os
 
+import click
+from requests_html import HTMLSession, urlparse
+
+
 GOOGLE_SEARCH_URL = "https://google.com/search"
 GOOGLE_SEARCH_PARAM = "q"
 GOOGLE_SEARCH_RESULT_WRAPPER_SELECTOR = "div.g"
 GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR = "video-voyager"
-SOCIAL_MEDIA_WEBSITES = ["twitter.com", "facebook.com","instagram.com", "linkedin.com", "reddit.com"]
+SOCIAL_MEDIA_WEBSITES = [
+    "twitter.com",
+    "facebook.com",
+    "instagram.com",
+    "linkedin.com",
+    "reddit.com",
+]
 DB_DIR_NAME = "Database"
+
 
 def get_base_website_url(url):
     parsed_url = urlparse(url)
@@ -25,11 +35,13 @@ def parse_website_search_results(html_elem):
     result_dict = defaultdict(list)
     social_media_result_list = []
     website_result_list = []
-    website_search_result_list = html_elem.find(
-        GOOGLE_SEARCH_RESULT_WRAPPER_SELECTOR)
+    website_search_result_list = html_elem.find(GOOGLE_SEARCH_RESULT_WRAPPER_SELECTOR)
 
     for website_search_result in website_search_result_list:
-        if website_search_result.element.getparent().tag == GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR:
+        if (
+            website_search_result.element.getparent().tag
+            == GOOGLE_SEARCH_RESULT_VIDEO_WRAPPER_SELECTOR
+        ):
             continue
 
         website_link_elem = website_search_result.find("a", first=True)
@@ -42,23 +54,18 @@ def parse_website_search_results(html_elem):
             website_title = website_link_elem.find("h3", first=True).text
         elif website_search_result.find("h3:last-child", first=True):
             website_title = website_title = website_search_result.find(
-                "h3:last-child",
-                first=True
+                "h3:last-child", first=True
             ).text
 
         if not all([website_title, website_link]):
             continue
 
-        result_dict[base_website_url].append({
-            "Title": website_title,
-            "URL": website_link
-        })
+        result_dict[base_website_url].append(
+            {"Title": website_title, "URL": website_link}
+        )
 
     for k, v in result_dict.items():
-        result = {
-            "Website": k,
-            "results": v
-        }
+        result = {"Website": k, "results": v}
         domain = get_root_domain(result["Website"])
         if domain in SOCIAL_MEDIA_WEBSITES:
             social_media_result_list.append(result)
@@ -85,22 +92,17 @@ def parse_video_search_results(html_elem):
         video_link = video_link_elem.attrs.get("href")
 
         if video_search_result.find("a[aria-label]", first=True):
-            video_thumbnail_elem = video_search_result.find(
-                "a[aria-label]",
-                first=True
-            )
+            video_thumbnail_elem = video_search_result.find("a[aria-label]", first=True)
         else:
             video_thumbnail_elem = video_search_result.find(
-                "div[aria-hidden='true']",
-                first=True
+                "div[aria-hidden='true']", first=True
             )
 
         video_title = ""
 
         if video_link_elem.find("div[role='heading'] span", first=True):
             video_title = video_link_elem.find(
-                "div[role='heading'] span",
-                first=True
+                "div[role='heading'] span", first=True
             ).text
         elif video_link_elem.find("h3", first=True):
             video_title = video_link_elem.find("h3", first=True).text
@@ -110,8 +112,7 @@ def parse_video_search_results(html_elem):
 
         if video_thumbnail_elem.find("div[role='presentation']", first=True):
             video_duration = video_thumbnail_elem.find(
-                "div[role='presentation']",
-                first=True
+                "div[role='presentation']", first=True
             ).text
 
         video_duration_in_seconds = convert_duration_to_seconds(video_duration)
@@ -119,35 +120,38 @@ def parse_video_search_results(html_elem):
         if not all([video_title, video_duration, video_link]):
             continue
 
-        result_dict[base_website_url].append({
-            "DurationInSeconds": video_duration_in_seconds,
-            "Title": video_title,
-            "URL": video_link
-        })
+        result_dict[base_website_url].append(
+            {
+                "DurationInSeconds": video_duration_in_seconds,
+                "Title": video_title,
+                "URL": video_link,
+            }
+        )
 
     for k, v in result_dict.items():
-        result_list.append({
-            "Website": k,
-            "results": v
-        })
+        result_list.append({"Website": k, "results": v})
 
     return result_list
 
 
-def main():
+@click.command()
+@click.option(
+    "--query",
+    "-q",
+    "search_query",
+    type=str,
+    prompt="Enter search query",
+    help="Search query",
+)
+def main(search_query):
     time_start = time.time()
-    search_query = "facebook"
     session = HTMLSession()
     output_dict = {
         "q": search_query,
-        "pageOneResultCount": '',
-        "pageOneVideoResultCount": '',
-        "timeTakenInMS": '',
-        "results": {
-            "Social Media": [],
-            "Webpages": [],
-            "Videos": []
-        }
+        "pageOneResultCount": "",
+        "pageOneVideoResultCount": "",
+        "timeTakenInMS": "",
+        "results": {"Social Media": [], "Webpages": [], "Videos": []},
     }
     response = session.get(
         url=GOOGLE_SEARCH_URL, params={GOOGLE_SEARCH_PARAM: search_query}
@@ -156,7 +160,10 @@ def main():
     if response and not hasattr(response, "html"):
         return
 
-    website_search_result_list, social_media_search_result_list = parse_website_search_results(response.html)
+    (
+        website_search_result_list,
+        social_media_search_result_list,
+    ) = parse_website_search_results(response.html)
     video_search_result_list = parse_video_search_results(response.html)
 
     time_end = time.time()
@@ -166,11 +173,13 @@ def main():
     output_dict["results"]["Social Media"] = social_media_search_result_list
     output_dict["results"]["Videos"] = video_search_result_list
     output_dict["pageOneResultCount"] = len(
-        website_search_result_list + social_media_search_result_list + video_search_result_list
+        website_search_result_list
+        + social_media_search_result_list
+        + video_search_result_list
     )
     output_dict["pageOneSocialMediaResultCount"] = len(social_media_search_result_list)
     output_dict["pageOneVideoResultCount"] = len(video_search_result_list)
-    output_dict["timeTakenInMS"] = total_time_in_ms    
+    output_dict["timeTakenInMS"] = total_time_in_ms
 
     file_name = f"{search_query}-{datetime.now().isoformat()}.json"
 
